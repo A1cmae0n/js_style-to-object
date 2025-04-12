fef6c38a5a9e752e5da48542918dd9345e5e6e09
Your Name <you@example.com>
2025-04-12 22:29:33 +0700

    attempt 1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssDeclarations = sourceString.split(';');
+  let obj = {};
+
+  for (const declaration of cssDeclarations) {
+    const colon = declaration.indexOf(':');
+
+    if (colon === 0) continue;
+
+    const property = declaration.substring(0, colon).trim();
+    const value = declaration.substring(colon + 1).trim();
+
+    // check if entry is not just whitespace
+    if (property !== '' && value !== '') {
+      obj[property] = value;
+    }
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

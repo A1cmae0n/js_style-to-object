f87f2538a76fb70baae80028e0721bec87c426db
Your Name <you@example.com>
2025-04-12 23:27:17 +0700

    changed reduce() for forEach()

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,25 +5,21 @@
  *
  * @return {object}
  */
+const result = {};
+
 function convertToObject(sourceString) {
   return sourceString
     .split(';')
     .map((declaration) => declaration.trim())
-    .filter((declaration) => declaration)
-    .reduce((result, declaration) => {
-      const colonPos = declaration.indexOf(':');
-      // move to the next declaration
-      if (colonPos === -1) return result;
-
-      const property = declaration.substring(0, colonPos).trim();
-      const value = declaration.substring(colonPos + 1).trim();
+    .filter((declaration) => declaration) // eliminates newline entries
+    .forEach((declaration) => {
+      const property = declaration.split(':')[0].trim();
+      const value = declaration.split(':')[1].trim();
 
       if (property && value) {
         result[property] = value;
       }
-
-      return result;
-    }, {});
+    });
 }
 
 module.exports = convertToObject;
